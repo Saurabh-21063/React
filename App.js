822670cbde540261447fcc98d474c6022a93a404
unknown <saurabhpalkvs5@gmail.com>
2024-02-11 12:36:28 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,27 @@
+// /**
+//  * insert a child element inside the parent div(nesting element)
+//  * <div id="parent">
+//  * <div id = "child">
+//  * <h1 > hello child!</h1> for sibling we can use [] array
+//  * </div>  for more complex nested html we use JSX
+//  * </.div>
+//  * <
+//  */
+// const object = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "hello children!"),
+//     React.createElement("h1", {}, "hello children!"),
+//   ])
+// );
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abd" },
+//   "hello React World!"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(object);
+// console.log(object);
+
+console.log("hello");
